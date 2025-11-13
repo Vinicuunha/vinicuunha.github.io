8a31e87f041a477c321799b3ee47b937809351ee
Vinicuunha <86085823+Vinicuunha@users.noreply.github.com>
2025-11-13 16:23:28 -0300

    update

--- src/pages/script_compatibilidade.js
+++ src/pages/script_compatibilidade.js
@@ -11,10 +11,10 @@ form.addEventListener('submit',(event) =>{
     console.log(valores);
 
     if(valores.jogo_desejado === "gta6"){
-        resultado_analise.innerHTML = '<img id="check" src="/src/imagens/check.png" alt="" width="60px">';
+        resultado_analise.innerHTML = '<img id="check" src="/src/pages/imagens/check.png" alt="" width="60px">';
         resultado_analise.innerHTML = resultado_analise.innerHTML + '<span style="color:#4caf50;">O jogo roda no seu computador</span>';
     }else{
-        resultado_analise.innerHTML = '<img id="check" src="/src/imagens/cancelar.png" alt="" width="60px">';
+        resultado_analise.innerHTML = '<img id="check" src="/src/pages/imagens/cancelar.png" alt="" width="70px">';
         resultado_analise.innerHTML = resultado_analise.innerHTML + '<span style="color:#fc202a;">O jogo não roda no seu computador</span>';
 
     }
