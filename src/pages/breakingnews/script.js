c7a65d5863d0cf4161c47629c608f61c39f2e722
Vinicuunha <86085823+Vinicuunha@users.noreply.github.com>
2025-11-13 16:10:38 -0300

    mudança de rotas

--- src/pages/breakingnews/script.js
+++
@@ -1,8 +0,0 @@
-const slider = document.getElementById('sliderFonte');
-const valor = document.getElementById('valorFonte');
-
-slider.addEventListener('input', () => {
-  const tamanho = slider.value + 'px';
-  document.body.style.fontSize = tamanho; // altera o tamanho da fonte do body
-  valor.textContent = tamanho; // atualiza o número exibido
-});
